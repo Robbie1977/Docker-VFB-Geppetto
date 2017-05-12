9a634445c4acd99cb228f8907d084b7ba011cf7e
Rob Court <robbie.court@gmail.com>
2017-05-12 10:48:53 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,6 +3,8 @@
 cd /opt/geppetto/org.geppetto/utilities/source_setup 
 python update_server.py
 
+echo 'Start of log...' >> /home/virgo/serviceability/logs/log.log
+
 tail -f /home/virgo/serviceability/logs/log.log & 
 
 /home/virgo/bin/startup.sh 
