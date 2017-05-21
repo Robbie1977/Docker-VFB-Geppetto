d5051a915ef8ec579198125008c5d62ec1e4765d
Rob Court <robbie.court@gmail.com>
2017-05-21 12:33:49 +0100

    creating log first

--- startup.sh
+++ startup.sh
@@ -3,8 +3,9 @@
 cd /opt/geppetto/org.geppetto/utilities/source_setup 
 python update_server.py
 
-echo 'Start of log...' >> /home/virgo/serviceability/logs/log.log
+mkdir -P ~/serviceability/logs
+echo 'Start of log...' > ~/serviceability/logs/log.log
 
-tail --sleep-interval=5 -F /home/virgo/serviceability/logs/log.log & 
+tail --sleep-interval=5 -F ~/serviceability/logs/log.log & 
 
 /home/virgo/bin/startup.sh 
