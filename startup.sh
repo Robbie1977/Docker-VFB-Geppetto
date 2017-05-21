34ada2df55e4fee156777cf9511ca5a4c56e0e63
Rob Court <robbie.court@gmail.com>
2017-05-21 12:28:23 +0100

    adding 5 sec sleep

--- startup.sh
+++ startup.sh
@@ -5,6 +5,6 @@ python update_server.py
 
 echo 'Start of log...' >> /home/virgo/serviceability/logs/log.log
 
-tail -F /home/virgo/serviceability/logs/log.log & 
+tail --sleep-interval=5 -F /home/virgo/serviceability/logs/log.log & 
 
 /home/virgo/bin/startup.sh 
