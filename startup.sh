f5ebeef7223c1982266db634a899ad8019107bdf
Rob Court <robbie.court@gmail.com>
2017-06-21 19:37:45 +0100

    typo fix

--- startup.sh
+++ startup.sh
@@ -8,7 +8,7 @@ echo 'Start of log...' > ~/serviceability/logs/log.log
 
 tail --sleep-interval=5 -F ~/serviceability/logs/log.log & 
 
-sed 's/XX:MaxPermSize=512m/-XX:MaxPermSize=$MAXSIZE/g' -i /home/virgo/bin/dmk.sh
+sed 's/XX:MaxPermSize=512m/XX:MaxPermSize=$MAXSIZE/g' -i /home/virgo/bin/dmk.sh
 sed 's/Xmx512m/Xmx$MAXSIZE/' -i /home/virgo/bin/dmk.sh
 
 /home/virgo/bin/startup.sh 
